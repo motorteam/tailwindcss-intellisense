726818267b326d65756b08b97c214fc2c671d196
William Tachau <wtachau@gmail.com>
2025-04-30 22:08:30 -0700

    rm exclusions

--- packages/vscode-tailwindcss/src/exclusions.d.ts
+++
@@ -1,4 +0,0 @@
-import { type WorkspaceConfiguration, type ConfigurationScope, type WorkspaceFolder } from 'vscode';
-export declare function getExcludePatterns(scope: ConfigurationScope | null): string[];
-export declare function isExcluded(file: string, folder: WorkspaceFolder): boolean;
-export declare function mergeExcludes(settings: WorkspaceConfiguration, scope: ConfigurationScope | null): any;
